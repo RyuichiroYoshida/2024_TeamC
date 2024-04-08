e79824272ffa01ff40bd3e55c7593b82e77fc10d
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-04-08 19:11:35 +0900

    [Add] CRIのテスト 検証済み

--- Assets/SoulRunProject/Scripts/Common/Core/Singleton/CriTest.cs
+++ Assets/SoulRunProject/Scripts/Common/Core/Singleton/CriTest.cs
@@ -7,15 +7,12 @@ namespace SoulRunProject
 {
     public class CriTest : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] private CriAudioManager.CueSheet cueSheet;
+        [SerializeField] private string cueName;
+
         void Start()
         {
-            CriAudioManager.Instance.PlaySE(CriAudioManager.CueSheet.Bgm, "SUMMER_TRIANGLE");
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
+            CriAudioManager.Instance.PlaySE(cueSheet, cueName);
         }
     }
 }
