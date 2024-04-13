69caa0c41c27c740b3cad365d9e2477f4fde6683
TsuyosiUsugi <qesulive@gmail.com>
2024-04-13 18:32:43 +0900

    [update] インゲームUI更新、テストで落ちたものが動くように

---
+++ Assets/SoulRunProject/Scripts/Test/Core/TestDropMover.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoulRunProject
+{
+    public class TestDropMover : MonoBehaviour
+    {
+        private float _speed = 10.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position += _speed * Time.deltaTime * -Vector3.forward;
+        }
+    }
+}
