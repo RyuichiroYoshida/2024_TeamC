543e37392c9f2c7800e1a835e70cd74926df0422
TsuyosiUsugi <qesulive@gmail.com>
2024-04-27 19:10:46 +0900

    [fix] 宣言で参照してたのを修正

--- Assets/SoulRunProject/Scripts/InGame/Manager/PlayerManager.cs
+++ Assets/SoulRunProject/Scripts/InGame/Manager/PlayerManager.cs
@@ -27,7 +27,7 @@ namespace SoulRunProject.Common
         private PlayerResourceContainer _resourceContainer;
         public FloatReactiveProperty CurrentHp { get; private set; }
         public PlayerResourceContainer ResourceContainer => _resourceContainer;
-        public float MaxHp => _currentStatus.Hp;
+        public float MaxHp;
         public Status CurrentStatus => _currentStatus;
         /// <summary>ダメージを無効化出来るかどうかの条件を格納するリスト</summary>
         public List<Func<bool>> IgnoreDamagePredicates { get; } = new();
@@ -35,6 +35,7 @@ namespace SoulRunProject.Common
         private Status _currentStatus;
         private void Awake()
         {
+            MaxHp = _currentStatus.Hp;
             _currentStatus = new Status(_baseStatus.Status);
             CurrentHp = new FloatReactiveProperty(_currentStatus.Hp);
             _inGameTimes = GetComponents<IPlayerPausable>();
