549283c9b4c301643f44a7101fa85ecdd2cfa0a4
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-05-24 18:56:32 +0900

    [Add] Scene切り替えでフェードできる

--- Assets/SoulRunProject/Scripts/Common/UI/LoadingScene.cs
+++ Assets/SoulRunProject/Scripts/Common/UI/LoadingScene.cs
@@ -5,43 +5,50 @@ using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using SoulRunProject.Common;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace SoulRunProject
 {
-    public class LoadingScene : MonoBehaviour
+    public class LoadingScene : AbstractSingletonMonoBehaviour<LoadingScene>
     {
         [Tooltip("ロード中に表示するUI")] [SerializeField]
-        private GameObject loadingUI;
+        private GameObject _loadingUI;
 
         [Tooltip("フェードに使用するマテリアル")] [SerializeField]
-        private Material fadeMaterial;
+        private Material _fadeMaterial;
 
         [Tooltip("フェードに使用するマスクテクスチャ")] [SerializeField]
-        private Texture maskTexture;
+        private Texture _maskTexture;
 
         [Tooltip("フェードの範囲")] [SerializeField, Range(0, 1)]
-        private float cutoutRange;
+        private float _cutoutRange;
 
-        [Tooltip("プログレスバー")] [SerializeField] private Slider slider;
-        [Tooltip("フェード時間")] [SerializeField] private float fadeDuration = 1.0f;
-        [Tooltip("最低ロード時間")] [SerializeField] private float minimumLoadTime = 2.0f;
+        [Tooltip("プログレスバー")] [SerializeField] private Slider _slider;
+        [Tooltip("フェード時間")] [SerializeField] private float _fadeDuration = 1.0f;
+        [Tooltip("最低ロード時間")] [SerializeField] private float _minimumLoadTime = 2.0f;
 
-        public static LoadingScene Instance { get; private set; }
+
+        protected override bool UseDontDestroyOnLoad => true;
         private AsyncOperation _async;
         private Action _onComplete;
 
-        private void Awake()
+        // プロパティIDをキャッシュ
+        private static readonly int MaskTex = Shader.PropertyToID("_MaskTex");
+        private static readonly int Range1 = Shader.PropertyToID("_Range");
+
+        private void OnValidate()
         {
-            if (Instance == null)
+            // エディタ上での変更を即時反映
+            if (_fadeMaterial != null)
             {
-                Instance = this;
-                DontDestroyOnLoad(this.gameObject);
+                _fadeMaterial.SetFloat(Range1, 1 - _cutoutRange);
             }
-            else
-            {
-                Destroy(this.gameObject);
-            }
+        }
+
+        private void Start()
+        {
+            _fadeMaterial.SetTexture(MaskTex, _maskTexture);
         }
 
         /// <summary> 次のシーンをロードする </summary>
@@ -49,14 +56,14 @@ namespace SoulRunProject
         {
             await FadeOut();
 
-            loadingUI.SetActive(true);
+            _loadingUI.SetActive(true);
             var startTime = Time.time;
             var targetProgress = 0f;
             var displayProgress = 0f;
 
             _async = await SceneController.Instance.LoadSceneAsync(sceneName);
 
-            while (Time.time - startTime < minimumLoadTime || _async is { isDone: false })
+            while (Time.time - startTime < _minimumLoadTime || _async is { isDone: false })
             {
                 if (_async != null)
                 {
@@ -65,13 +72,13 @@ namespace SoulRunProject
 
                 // プログレスバーを滑らかに進める
                 displayProgress = Mathf.MoveTowards(
-                    displayProgress, targetProgress, Time.deltaTime / minimumLoadTime);
-                slider.value = displayProgress;
+                    displayProgress, targetProgress, Time.deltaTime / _minimumLoadTime);
+                _slider.value = displayProgress;
 
                 await UniTask.DelayFrame(1);
             }
 
-            loadingUI.SetActive(false);
+            _loadingUI.SetActive(false);
             _onComplete?.Invoke();
 
             await FadeIn();
@@ -80,24 +87,22 @@ namespace SoulRunProject
         /// <summary> ロード完了時に実行する処理を登録する </summary>
         private async UniTask FadeOut()
         {
-            fadeMaterial.SetTexture("_MaskTex", maskTexture);
-
             // DOTweenを使用してRangeパラメータをアニメーション
-            DOTween.To(() => cutoutRange, x => cutoutRange = x, 1, fadeDuration)
-                .OnUpdate(() => fadeMaterial.SetFloat("_Range", 1 - cutoutRange))
+            DOTween.To(() => _cutoutRange, x => _cutoutRange = x, 1, _fadeDuration)
+                .OnUpdate(() => _fadeMaterial.SetFloat(Range1, 1 - _cutoutRange))
                 .SetEase(Ease.Linear);
 
             // フェードアウトが終わるまで待機する
-            await UniTask.Delay(TimeSpan.FromSeconds(fadeDuration));
+            await UniTask.Delay(TimeSpan.FromSeconds(_fadeDuration));
         }
 
         private async UniTask FadeIn()
         {
             // DOTweenアニメーションを待機する
-            DOTween.To(() => cutoutRange, x => cutoutRange = x, 0, fadeDuration)
-                .OnUpdate(() => fadeMaterial.SetFloat("_Range", 1 - cutoutRange))
+            DOTween.To(() => _cutoutRange, x => _cutoutRange = x, 0, _fadeDuration)
+                .OnUpdate(() => _fadeMaterial.SetFloat(Range1, 1 - _cutoutRange))
                 .SetEase(Ease.Linear);
-            await UniTask.Delay(TimeSpan.FromSeconds(fadeDuration));
+            await UniTask.Delay(TimeSpan.FromSeconds(_fadeDuration));
         }
     }
 }
