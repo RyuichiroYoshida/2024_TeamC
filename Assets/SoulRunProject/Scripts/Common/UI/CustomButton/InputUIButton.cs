2fa57a2973bee75bfe5517e7ebd170fc7a57af22
TsuyoshiUsugi <qesulive@gmail.com>
2024-06-12 16:41:26 +0900

    [update] ボタンにポインターが重なったときの処理等を追加

--- Assets/SoulRunProject/Scripts/Common/UI/CustomButton/InputUIButton.cs
+++ Assets/SoulRunProject/Scripts/Common/UI/CustomButton/InputUIButton.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using DG.Tweening;
+using SoulRunProject.Common;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace SoulRun.InGame
 {
     /// <summary> ボタンの表示状態を管理するボタンクラス View</summary>
     [RequireComponent(typeof(CanvasGroup))]
-    public class InputUIButton : InputUIButtonBase
+    public class InputUIButton : InputUIButtonBase, IPointerEnterHandler, IPointerExitHandler
     {
         private CanvasGroup _button;
         private Vector3 _originalScale;
+        private float _fadeTime = 0.2f;
 
         // イベントの定義
         public event Action OnButtonDown;
@@ -40,8 +43,9 @@ namespace SoulRun.InGame
 
         protected override void OnPointerDownEvent()
         {
+            CriAudioManager.Instance.PlaySE("SE_Decision");
             // DOTweenを使ってスケールを小さくするアニメーションを実行
-            transform.DOScale(_originalScale * 0.8f, 0.2f).SetLink(gameObject);
+            transform.DOScale(_originalScale * 0.8f, _fadeTime).SetLink(gameObject);
             _button.alpha = 0.5f;
             // イベントの発火
             OnButtonDown?.Invoke();
@@ -50,12 +54,23 @@ namespace SoulRun.InGame
         protected override void OnPointerUpEvent()
         {
             // DOTweenを使ってスケールを元に戻すアニメーションを実行
-            transform.DOScale(_originalScale, 0.2f).SetLink(gameObject);
+            transform.DOScale(_originalScale, _fadeTime).SetLink(gameObject);
             _button.alpha = 1f;
             // イベントの発火
             OnButtonUp?.Invoke();
             // クリックイベントの発火
             onClick?.Invoke(this);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            CriAudioManager.Instance.PlaySE("SE_Select");
+            transform.DOScale(_originalScale * 1.2f, _fadeTime).SetLink(gameObject);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            transform.DOScale(_originalScale, _fadeTime).SetLink(gameObject);
         }
     }
 }
