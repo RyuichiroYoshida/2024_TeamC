bcb9d7be2289ec527dbeac081206eae875b92cb0
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-08-25 18:08:08 +0900

    [Add] Fade Scenemanager

---
+++ Assets/SoulRunProject/Scripts/Common/UI/Fade/IFadeStrategy.cs
@@ -0,0 +1,11 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace HikanyanLaboratory.Fade
+{
+    public interface IFadeStrategy
+    {
+        UniTask FadeOut(Material fadeMaterial);
+        UniTask FadeIn(Material fadeMaterial);
+    }
+}
