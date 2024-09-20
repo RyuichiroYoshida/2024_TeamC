4f4cf7a5ee745873d97b849053fc7ee007b85fd3
Hikanyan <hikanyan0131@gmail.com>
2024-09-21 03:24:18 +0900

    update : SEだけ同時再生数の上限を10個に設定

--- Assets/SoulRunProject/Scripts/Common/Core/Singleton/Audio/SEPlayer.cs
+++ Assets/SoulRunProject/Scripts/Common/Core/Singleton/Audio/SEPlayer.cs
@@ -1,10 +1,17 @@
-﻿using CriWare;
+﻿using System;
+using System.Collections.Generic;
+using CriWare;
+using Cysharp.Threading.Tasks;
 using UniRx;
+using UnityEngine;
 
 namespace SoulRunProject.Audio
 {
     public class SEPlayer : CriAudioPlayerService
     {
+        private readonly int maxSameSePlayCount = 10; // 同時再生数の上限
+        private readonly Dictionary<string, int> sePlayCountDict = new Dictionary<string, int>(); // SEの再生回数をカウント
+
         private readonly CompositeDisposable _disposables = new CompositeDisposable();
 
         public SEPlayer(string cueSheetName, CriAtomListener listener)
@@ -18,6 +25,37 @@ namespace SoulRunProject.Audio
         ~SEPlayer()
         {
             _disposables.Dispose();
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        public override Guid Play(string cueName, float volume = 1f, bool isLoop = false)
+        {
+            // SEの再生回数を確認
+            if (!sePlayCountDict.ContainsKey(cueName))
+            {
+                sePlayCountDict[cueName] = 0;
+            }
+
+            // 同じSEの再生回数が上限に達している場合、再生しない
+            if (sePlayCountDict[cueName] >= maxSameSePlayCount)
+            {
+                Debug.LogWarning($"SE {cueName} の再生が制限されました（上限: {maxSameSePlayCount}）");
+                return Guid.Empty;
+            }
+
+            // 通常のSE再生処理
+            Guid playbackId = base.Play(cueName, volume, isLoop);
+            if (playbackId == Guid.Empty) return playbackId;
+            sePlayCountDict[cueName]++;
+
+            // 再生終了時にカウントを減少
+            var playback = _playbacks[playbackId];
+            Observable.EveryUpdate()
+                .Where(_ => playback.GetStatus() == CriAtomExPlayback.Status.Removed)
+                .Take(1)
+                .Subscribe(_ => { sePlayCountDict[cueName]--; }).AddTo(_disposables);
+
+            return playbackId;
         }
     }
 }
