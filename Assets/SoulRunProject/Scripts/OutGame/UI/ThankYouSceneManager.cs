a81ee73b0b638d395b9359e586569b319dc35549
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2024-09-22 16:31:11 +0900

    [add] ThankYouForPlayingのボイス追加。 [add] 鷹の攻撃音追加。

--- Assets/SoulRunProject/Scripts/OutGame/UI/ThankYouSceneManager.cs
+++ Assets/SoulRunProject/Scripts/OutGame/UI/ThankYouSceneManager.cs
@@ -9,11 +9,13 @@ namespace SoulRunProject
     {
         [SerializeField, CustomLabel("タイトルに遷移するまでの時間")] private float _duration;
         [SerializeField] private string _bgmName;
+        [SerializeField] private string _voiceName = "VOICE_GameEnd_1";
 
         private void Start()
         {
             Invoke(nameof(ToTitle), _duration);
             CriAudioManager.Instance.Play(CriAudioType.CueSheet_BGM, _bgmName);
+            CriAudioManager.Instance.Play(CriAudioType.CueSheet_VOICE, _voiceName);
         }
 
         void ToTitle()
